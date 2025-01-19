fa015267b09cdddc3139efc62e3083ba4b242904
jmach <jmach@GWTN156-11>
2025-01-19 07:11:58 -0600

    Add project files.

---
+++ Wk1Ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk1Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // print out input Week 1 Ex1
+            Console.WriteLine("Welcome to ITSE1302");
+        }
+    }
+}
